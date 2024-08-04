a3d909b30251886d7705eafacb14eb33f44a0371
Evgeniya <evg.semiletova@gmail.com>
2024-08-05 00:34:00 +0300

    fixed errors with comparison symbols

--- guess.py
+++ guess.py
@@ -11,12 +11,18 @@ for guesses_taken in range(6):
     print("try to guess")
     guess = int(input())
 
-    if guess < number:
+    if guess > number:
         print("your number is bigger than mine")
 
-    if guess > number:
+    if guess < number:
         print("your number is smaller than mine")
 
     if guess == number:
         break
 
+if guess == number:
+    guesses_taken = str(guesses_taken + 1)
+    print(f"great {name}, you did it in {guesses_taken} tries")
+
+if guess != number:
+    print(f"Alas, I had a number {number} in mind")
